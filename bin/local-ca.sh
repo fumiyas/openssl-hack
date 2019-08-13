bd7ad34ef18c3cd505b01110cda31724f7034eb0
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2019-08-13 17:43:45 +0900

    refactor: local-ca: Execute openssl-ca.bash if openssl-ca does not exist

--- bin/local-ca.sh
+++ bin/local-ca.sh
@@ -2,4 +2,8 @@
 CA_ARGV0="$0"
 CA_DIR="/usr/local/etc/ca"
 export CA_ARGV0 CA_DIR
-exec "/usr/local/bin/openssl-ca" "$@"
+if [ -x "${0%/*}/openssl-ca" ]; then
+  exec "${0%/*}/openssl-ca" "$@"
+else
+  exec "${0%/*}/openssl-ca.bash" "$@"
+fi
